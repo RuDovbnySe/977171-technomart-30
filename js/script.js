0ea9872463633e98fa846331e8f87b2e86e81fe8
Сергей Довбня <65117604+RuDovbnySe@users.noreply.github.com>
2021-03-02 01:14:50 +0300

    Доработал замечания

--- js/script.js
+++ js/script.js
@@ -13,7 +13,20 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("Клик - закрыть");
   letter.classList.remove("letter-show");
+  map.classList.remove("popup-map-show");
 })
 
+var map_open = document.querySelector(".contacts-map");
+var map = document.querySelector(".popup-map");
+var map_close = document.querySelector(".popup-map-close")
 
+map_open.addEventListener("click", function (evt){
+  evt.preventDefault();
+  map.classList.add("popup-map-show");
+})
 
+map_close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Клик - закрыть");
+  map.classList.remove("popup-map-show");
+})
